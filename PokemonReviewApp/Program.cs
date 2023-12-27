7496bb7ac0990fd4bd25c9bca4e62cc4c9e03d9e
sylgeek <sylgeekpro@outlook.com>
2023-12-27 22:05:42 +0400

    add CI/CD

--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -48,11 +48,10 @@ void SeedData(IHost app)
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
